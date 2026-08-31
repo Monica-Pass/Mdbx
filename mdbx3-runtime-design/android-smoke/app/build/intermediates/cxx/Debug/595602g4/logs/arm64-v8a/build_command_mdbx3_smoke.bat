73e0c899d023d450aa5f68e4915fc16b930fbab0
JoyinJoester <lichaoran8@gmail.com>
2026-08-31 23:25:34 +0800

    chore: ignore Android smoke build outputs

--- mdbx3-runtime-design/android-smoke/app/build/intermediates/cxx/Debug/595602g4/logs/arm64-v8a/build_command_mdbx3_smoke.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\AndroidSDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\joyins\\Desktop\\Monica-all\\mdbx\\mdbx3-runtime-design\\android-smoke\\app\\.cxx\\Debug\\595602g4\\arm64-v8a" ^
-  mdbx3_smoke
--- mdbx3-runtime-design/android-smoke/app/build/intermediates/cxx/Debug/595602g4/logs/x86/build_command_mdbx3_smoke.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\AndroidSDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\joyins\\Desktop\\Monica-all\\mdbx\\mdbx3-runtime-design\\android-smoke\\app\\.cxx\\Debug\\595602g4\\x86" ^
-  mdbx3_smoke
